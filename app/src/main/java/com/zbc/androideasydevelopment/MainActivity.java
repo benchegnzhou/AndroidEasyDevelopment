a9a806c30d0202674fb0ee3d35d5ab1f1f23e1bd
benchegnzhou <1107472286@qq.com>
2019-07-05 15:43:13 +0800

    项目的第一滴血

---
+++ app/src/main/java/com/zbc/androideasydevelopment/MainActivity.java
@@ -0,0 +1,19 @@
+package com.zbc.androideasydevelopment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by benchengzhou on 2019/7/5  15:27 .
+ * 作者邮箱： mappstore@163.com
+ * 功能描述： 测试首页
+ * 类    名： MainActivity
+ * 备    注：
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
