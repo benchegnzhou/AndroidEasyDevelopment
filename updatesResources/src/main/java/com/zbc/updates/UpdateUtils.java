bd40c73b243600c4ea9dd6b650b5dfc7ddb2cff1
benchegnzhou <1107472286@qq.com>
2019-07-16 17:58:44 +0800

    完成基础配置

---
+++ updatesResources/src/main/java/com/zbc/updates/UpdateUtils.java
@@ -0,0 +1,14 @@
+package com.zbc.updates;
+/**
+ * Created by benchengzhou on 2019/7/16  17:14 .
+ * 作者邮箱： mappstore@163.com
+ * 功能描述：
+ * 类    名： UpdateUtil
+ * 备    注：
+ */
+
+public class UpdateUtils {
+
+
+
+}
